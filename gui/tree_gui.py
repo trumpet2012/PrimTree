df22c19de190c247db0fa7ac09ab11083d899436
Trent Holliday <tholliday@morristechnology.com>
2015-11-24 20:08:37 -0500

    Pushing so that my grid info is saved.

--- gui/tree_gui.py
+++ gui/tree_gui.py
@@ -6,6 +6,13 @@ from itertools import cycle
 from tree.node import Node
 
 
+def node_clicked(event, node):
+    node.node_state.set(1)
+    node.master.create_line(node.canvas.winfo_x(), node.canvas.winfo_y(), node.canvas.winfo_x()+100, node.canvas.winfo_y(), fill='black')
+    node.canvas.create_rectangle(node.canvas.winfo_x()-100,node.canvas.winfo_y()-100,node.canvas.winfo_x(),node.canvas.winfo_y(), fill='black')
+
+
+
 class NodeFrame(tk.Frame):
 
     num_nodes = 10
@@ -14,7 +21,10 @@ class NodeFrame(tk.Frame):
         super().__init__(master=master)
 
         self.canvas = tk.Canvas()
+
         self.nodes = self.create_nodes()
+
+        # self.canvas.create_line(0, 0, 500, 500, fill='black')
 
     def create_nodes(self):
         nodes = []
@@ -23,10 +33,15 @@ class NodeFrame(tk.Frame):
             x = random.randint(0, 20)
             y = random.randint(0, 20)
             print(x, y)
-            node = Node(self, index, x, y)
+
+            node = Node(self.canvas, index, x, y)
+            node.canvas.bind('<Button-1>', lambda event, node=node: node_clicked(event, node))
+            node.canvas.grid(row=y, column=x, padx=10, pady=10)
+
             nodes.append(node)
 
         return nodes
+
 
 gui = NodeFrame()
 
--- tree/node.py
+++ tree/node.py
@@ -1,20 +1,35 @@
+import random
 import tkinter as tk
 
 
 class Node(tk.Frame):
     radius = 30
 
-    def __init__(self, parent, label, x, y):
+    def __init__(self, parent, name, x, y):
         super().__init__(parent, cnf={})
-        size = self.radius*2
+        size = self.radius * 2
         self.canvas = tk.Canvas(width=size, height=size)
         self.x = x
         self.y = y
-        radius = self.radius
+        self.name = name
+        self.node_state = tk.IntVar()
+        self.node_color = tk.StringVar()
+        self.node_color.set('#ccc')
 
-        self.canvas.create_oval(5, 5, 60, 60, fill="green")
-        self.canvas.create_text(30, 30, text=label)
-        self.canvas.grid(row=y, column=x, padx=10, pady=10)
+        def node_state_change(node_state, node_color, *args):
+            if node_state.get() == 0:
+                node_color.set('#ccc')
+            elif node_state.get() == 1:
+                node_color.set('#f00')
+            # Redraw the node with the updated color
+            self.draw_node()
+
+        self.node_state.trace_variable('w', lambda *args: node_state_change(self.node_state, self.node_color))
+        self.draw_node()
+
+    def draw_node(self):
+        self.canvas.create_oval(5, 5, 60, 60, fill=self.node_color.get(), outline='white')
+        self.canvas.create_text(30, 30, text=self.name)
 
     def contains(self, x, y):
         radius = self.radius
@@ -26,3 +41,6 @@ class Node(tk.Frame):
         radius = self.radius
 
         return x - radius, y - radius, x + radius, y + radius
+
+    def mark_visited(self):
+        self.node_state = 1
